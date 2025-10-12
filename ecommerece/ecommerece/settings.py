6497ad4fb8e2e65135e25269d1663e3cabac3b52
umairehsan6 <umairehsan59@gmail.com>
2025-10-13 01:59:15 +0500

    Deployment Fixes

--- ecommerece/ecommerece/settings.py
+++ ecommerece/ecommerece/settings.py
@@ -24,7 +24,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+# Parse ALLOWED_HOSTS from environment variable
+allowed_hosts_env = os.environ.get('ALLOWED_HOSTS', '')
+if allowed_hosts_env:
+    ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_env.split(',') if host.strip()]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
