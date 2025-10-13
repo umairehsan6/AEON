b60d14a57c62b3a29dc45ec15159a2aef9ed45b4
umairehsan6 <umairehsan59@gmail.com>
2025-10-13 05:26:25 +0500

    Vercel

--- ecommerece/ecommerece/settings.py
+++ ecommerece/ecommerece/settings.py
@@ -187,6 +187,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://aeon-ecom1.vercel.app",  # Add your Vercel domain here
 ]
 
 # Add CORS origins from environment variable
